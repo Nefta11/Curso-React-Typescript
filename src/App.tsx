952c93d3ffd50e558ca12ec11f61474b8a5d6d34
Neftali Arturo Hernández Vergara <132298011+Nefta11@users.noreply.github.com>
2024-05-10 22:34:45 -0600

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BasicTypes } from "./typescript"
+import ObjectLiterals from "./typescript/ObjectLiterals"
 
 function App() {
 
@@ -6,6 +7,9 @@ function App() {
     <main>
 <h1>Introduccion a TS - React</h1>
 <BasicTypes/>
+<br />
+<br />
+<ObjectLiterals/>
     </main>
   )
 }
---
+++ src/typescript/ObjectLiterals.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ObjectLiterals = () => {
+  return (
+    <div>
+      ObjectLiterals
+    </div>
+  )
+}
+
+export default ObjectLiterals
