bca70f896401624e5924449cf5c171edbb96cd51
Neftali Arturo Hernández Vergara <132298011+Nefta11@users.noreply.github.com>
2024-05-20 10:32:10 -0600

    Updated

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,15 +2,22 @@ import { useState } from "react"
 
 const Counter = () => {
   
-const [first,setfirst]=useState(second)
-
+const [count , setCount] = useState(10);
   
+  const increaseBy =(value: number) =>{
+    setCount(count  + value);
+  }
+
     return (
+    <>
+     <h3>Contador: <small>{count}</small></h3> 
 
-
-    <div>
-      Counter
-    </div>
+     <div>
+      <button onClick={()=>increaseBy(+1)} >+1</button>
+      &nbsp;
+      <button onClick={()=>increaseBy(-1)}>-1</button>
+     </div>
+    </>
   )
 }
 
