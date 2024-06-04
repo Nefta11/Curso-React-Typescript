af6565e37b10839038f4b706bfbd1ca858d6fb2c
Neftali Arturo Hernández Vergara <132298011+Nefta11@users.noreply.github.com>
2024-06-04 16:21:12 -0600

    Updated

--- src/components/FormsPage.tsx
+++ src/components/FormsPage.tsx
@@ -1,46 +1,43 @@
-import { useForm } from "react-hook-form"
-
+import { useForm } from "react-hook-form";
 
 type FormInputs = {
-        email: string,
-        password:string
-}
-
+  email: string;
+  password: string;
+};
 
 const FormsPage = () => {
+  const { register, handleSubmit, formState, watch } = useForm<FormInputs>({
+    defaultValues: {
+      email: 'neftali@gmail.com',
+      password: '1234567'
+    }
+  });
 
+  const onSubmit = (data: FormInputs) => {
+    console.log({ data });
+  };
 
-    
-    const {register} = useForm<FormInputs>({
-        defaultValues:{
-            email: 'neftali@gmail.com',
-            password:'1234567'
-        }
-
-    });
+console.log(watch('email'));
 
 
   return (
     <>
-      
-    <form action="">
-    <h3>Formularios</h3>
-
-    <div style={{display:'flex', flexDirection:'column'}}>
-        <input type="text" placeholder="Email" {...register('email')}/>
-        <input type="text" placeholder="Password"  {...register('password')}/>
-
-
-        <button type="submit">Ingresar</button>
-
-    </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h3>Formularios</h3>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <input type="text" placeholder="Email" {...register('email', {required: true})}/>
+          <input type="password" placeholder="Password" {...register('password')} />
+          <button type="submit">Ingresar</button>
+        </div>
 
 
-    </form>
+      </form>
 
-
+      <pre>
+        {JSON.stringify( formState, null, 2)}
+      </pre>
     </>
-  )
-}
+  );
+};
 
-export default FormsPage
+export default FormsPage;
