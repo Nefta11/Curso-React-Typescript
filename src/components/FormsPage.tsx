61a9e5c9157d10580c4a85f12fc48ca28aa7809d
Neftali Arturo Hernández Vergara <132298011+Nefta11@users.noreply.github.com>
2024-06-04 15:39:35 -0600

    Add: Forms

---
+++ src/components/FormsPage.tsx
@@ -0,0 +1,32 @@
+
+
+const FormsPage = () => {
+
+
+
+
+
+  return (
+    <>
+      
+    <form action="">
+    <h3>Formularios</h3>
+
+    <div style={{display:'flex', flexDirection:'column'}}>
+        <input type="text" placeholder="Email" />
+        <input type="text" placeholder="Password" />
+
+
+        <button type="submit">Ingresar</button>
+
+    </div>
+
+
+    </form>
+
+
+    </>
+  )
+}
+
+export default FormsPage
