cfcb30d5dc92a7233bc7ae9698926fb4ec32b7c3
Neftali Arturo Hernández Vergara <132298011+Nefta11@users.noreply.github.com>
2024-07-09 23:02:28 -0600

    Update Counter.tsx

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -7,7 +7,7 @@ const [count , setCount] = useState(10);
   const increaseBy =(value: number) =>{
     setCount(count  + value);
   }
-
+  
     return (
     <>
      <h3>Contador: <small>{count}</small></h3> 
